f8dff56fb72ca0a4b82e0dd1c3a4044f21f49145
Biiigmat <matinmoein511@gmail.com>
2024-07-21 18:29:58 +0330

    final

--- Trial 3/project/blog/urls.py
+++ Trial 3/project/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import home_page , about_page
+from .views import home_page , about_page , contact_page , bloghome_page , blogsingle_page , elements_page
+
+app_name='blog'
 
 urlpatterns = [
-    path("" , home_page),
-    path("about",about_page),
+    path("" , home_page , name='index'),
+    path("about",about_page , name='about'),
+    path('contact' , contact_page ,name='contact'),
+    path('blog-home' , bloghome_page , name='blog-home'),
+    path('blog-single' , blogsingle_page , name='blog-single'),
+    path('elements' ,elements_page , name='elements'),
 ]
--- Trial 3/project/blog/views.py
+++ Trial 3/project/blog/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page (request):
-    return render (request , 'index.html')
+    return render (request , 'blog/index.html')
 
 def about_page(request):
-    return render (request , 'about.html')
+    return render (request , 'blog/about.html')
+
+def contact_page(request):
+    return render (request , 'blog/contact.html')
+
+def blogsingle_page(request):
+    return render (request , 'blog/blog-single.html')
+
+def bloghome_page(request):
+    return render (request , 'blog/blog-home.html')
+
+def elements_page(request):
+    return render (request , 'blog/elements.html')
